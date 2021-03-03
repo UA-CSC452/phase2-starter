f4e4ab88a219c3cddd8333eefe81a92591ce4348
John H. Hartman <john.hartman@gmail.com>
2021-03-02 19:47:49 -0700

    updated for 2021

--- phase2.h
+++ phase2.h
@@ -20,14 +20,14 @@
 extern  int	    P2_Sleep(int seconds) CHECKRETURN;
 
 
-extern  int     P2_DiskRead(int unit, int track, int first, int sectors, void *buffer) CHECKRETURN;
-extern  int	    P2_DiskWrite(int unit, int track, int first, int sectors, void *buffer) CHECKRETURN;
-extern  int 	P2_DiskSize(int unit, int *sector, int *track, int *disk) CHECKRETURN;
+extern  int     P2_DiskRead(int unit, int first, int sectors, void *buffer) CHECKRETURN;
+extern  int	    P2_DiskWrite(int unit, int first, int sectors, void *buffer) CHECKRETURN;
+extern  int 	P2_DiskSize(int unit, int *sector, int *disk) CHECKRETURN;
 
 extern  int     P2_Spawn(char *name, int (*func)(void *arg), void *arg, int stackSize, 
                          int priority, int *pid) CHECKRETURN;
 extern  int     P2_Wait(int *pid, int *status) CHECKRETURN;
-extern  void    P2_Terminate(int status);
+extern  int     P2_Terminate(int status);
 extern  int     P2_SetSyscallHandler(unsigned int number, 
                         void (*handler)(USLOSS_Sysargs *args)) CHECKRETURN;
 
@@ -40,12 +40,12 @@ extern	int 	P3_Startup(void *) CHECKRETURN;
  */
 
 
-#define P2_INVALID_SYSCALL      -20
-#define P2_INVALID_SECONDS      -21
-#define P2_INVALID_TRACK        -22
-#define P2_INVALID_FIRST        -23
-#define P2_INVALID_SECTORS      -24
-#define P2_NULL_ADDRESS         -25
+#define P2_INVALID_SYSCALL      -25
+#define P2_INVALID_SECONDS      -26
+#define P2_INVALID_FIRST        -27
+#define P2_INVALID_SECTORS      -28
+#define P2_NULL_ADDRESS         -29
+#define P2_NOT_SPAWNED          -30
 
 #endif
 
